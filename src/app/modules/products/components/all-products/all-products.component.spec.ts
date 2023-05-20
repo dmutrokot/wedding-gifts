feeeff527f1b91e64a7c200adbf24c7f52672b21
airmangataos <dmutrokot94@gmail.com>
2023-05-20 19:58:26 +0300

    feat: Add tests to AllProductsComponent

--- src/app/modules/products/components/all-products/all-products.component.spec.ts
+++ src/app/modules/products/components/all-products/all-products.component.spec.ts
@@ -1,20 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { of } from 'rxjs';
+
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models';
 
 import { AllProductsComponent } from './all-products.component';
-import { ProductsService } from '../../services/products.service';
 
 describe('AllProductsComponent', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
+  let component: AllProductsComponent;
+  let fixture: ComponentFixture<AllProductsComponent>;
+  let productsService: ProductsService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [AllProductsComponent],
       providers: [ProductsService],
-    })
-  );
+    }).compileComponents();
+
+    productsService = TestBed.inject(ProductsService);
+    fixture = TestBed.createComponent(AllProductsComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create', () => {
-    const fixture = TestBed.createComponent(AllProductsComponent);
-    const app = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
 
-    expect(app).toBeTruthy();
+  describe('#ngOnInit', () => {
+    it('should call #getProducts', () => {
+      const mockProducts: Product[] = [];
+      spyOn(productsService, 'getProducts').and.returnValue(of(mockProducts));
+
+      component.ngOnInit();
+
+      expect(productsService.getProducts).toHaveBeenCalled();
+    });
+
+    it('should set @allProducts', () => {
+      const mockProducts: Product[] = [
+        {
+          id: 1,
+          name: 'Tea pot',
+          brand: 'Le Creuset',
+          price: '47.00GBP',
+          in_stock_quantity: 50,
+        },
+      ];
+      spyOn(productsService, 'getProducts').and.returnValue(of(mockProducts));
+
+      component.ngOnInit();
+
+      expect(component.allProducts).toEqual(mockProducts);
+    });
   });
 });
