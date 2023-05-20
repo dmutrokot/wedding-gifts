ef467175d4b6000868cd72f1c214e99536427729
airmangataos <dmutrokot94@gmail.com>
2023-05-20 16:56:41 +0300

    feat: Add Product model

---
+++ src/app/modules/products/models/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  price: string;
+  in_stock_quantity: number;
+}
--- src/app/modules/products/services/products.service.ts
+++ src/app/modules/products/services/products.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { Product } from '../models';
+
 import * as products from '../../../../data/products.json';
 
 @Injectable()
 export class ProductsService {
-  public getProducts() {
+  public getProducts(): Product[] {
     return products;
   }
 }
