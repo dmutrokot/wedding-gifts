2ac9ea46563cdffa4fa983e63f816e5da8f8df3b
airmangataos <dmutrokot94@gmail.com>
2023-05-21 22:45:34 +0300

    feat: Handle query params for products lists

--- src/app/modules/products/containers/products/products.component.spec.ts
+++ src/app/modules/products/containers/products/products.component.spec.ts
@@ -1,19 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { ProductsService } from '../../services/products.service';
+import { of } from 'rxjs';
 
-import { ProductsComponent } from './products.component';
+import { ProductsService } from '../../services/products.service';
 import { AllProductsComponent } from '../../components/all-products/all-products.component';
 import { PurchasedProductsComponent } from '../../components/purchased-products/purchased-products.component';
 import { UnpurchasedProductsComponent } from '../../components/unpurchased-products/unpurchased-products.component';
 import { SelectedProductsComponent } from '../../components/selected-products/selected-products.component';
 import { ProductListType } from '../../constants';
 
+import { ProductsComponent } from './products.component';
+
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
-  let productsService: ProductsService;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,12 +29,28 @@ describe('ProductsComponent', () => {
         SelectedProductsComponent,
         UnpurchasedProductsComponent,
       ],
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({ activeView: ProductListType.All }),
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate'),
+          },
+        },
+      ],
     }).compileComponents();
 
-    productsService = TestBed.inject(ProductsService);
+    router = TestBed.inject(Router);
+    activatedRoute = TestBed.inject(ActivatedRoute);
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -39,19 +59,11 @@ describe('ProductsComponent', () => {
 
   it('#isActive should return true when the view equals activeView', () => {
     component.activeView = ProductListType.All;
-
     expect(component.isActive(ProductListType.All)).toBeTrue();
   });
 
   it('#isActive should return false when the view does not equal activeView', () => {
     component.activeView = ProductListType.All;
-
     expect(component.isActive(ProductListType.Purchased)).toBeFalse();
-  });
-
-  it('#onSetActiveView should change the value of activeView', () => {
-    component.onSetActiveView(ProductListType.Purchased);
-
-    expect(component.activeView).toEqual(ProductListType.Purchased);
   });
 });
--- src/app/modules/products/containers/products/products.component.ts
+++ src/app/modules/products/containers/products/products.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { takeUntil } from 'rxjs';
+
+import { Unsubscriber } from '@core/classes/unsubscriber';
 
 import { ProductListType } from '../../constants';
 
@@ -7,15 +12,39 @@ import { ProductListType } from '../../constants';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent extends Unsubscriber implements OnInit {
   public activeView: ProductListType = ProductListType.All;
   public readonly ProductListType = ProductListType;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.pipe(takeUntil(this.destroy$)).subscribe((params) => {
+      this.activeView = params['activeView'] as ProductListType;
+
+      if (!this.activeView) {
+        this.activeView = ProductListType.All;
+
+        this.router.navigate([], {
+          relativeTo: this.activatedRoute,
+          queryParams: { activeView: this.activeView },
+          queryParamsHandling: 'merge',
+        });
+      }
+    });
+  }
 
   public isActive(view: ProductListType): boolean {
     return this.activeView === view;
   }
 
   public onSetActiveView(view: ProductListType): void {
-    this.activeView = view;
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: { activeView: view },
+      queryParamsHandling: 'merge',
+    });
   }
 }
