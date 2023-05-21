f213801cf341a0b0d6a6f77f04f1303e9cfcd74a
airmangataos <dmutrokot94@gmail.com>
2023-05-21 10:47:27 +0300

    feat: Add spec for PurchasedProductsComponent

--- src/app/modules/products/components/purchased-products/purchased-products.component.spec.ts
+++ src/app/modules/products/components/purchased-products/purchased-products.component.spec.ts
@@ -1,26 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductsService } from '../../services/products.service';
+import { BehaviorSubject } from 'rxjs';
 
+import { ProductsService } from '../../services/products.service';
 import { PurchasedProductsComponent } from './purchased-products.component';
+import { SelectedProduct } from '../../models';
 
 describe('PurchasedProductsComponent', () => {
   let component: PurchasedProductsComponent;
   let fixture: ComponentFixture<PurchasedProductsComponent>;
-  let productsService: ProductsService;
+
+  const mockProductList = new BehaviorSubject<SelectedProduct[]>([
+    { id: 1, name: 'product1', brand: 'brand1', price: '20.00GBP', quantity: 2 },
+    { id: 2, name: 'product2', brand: 'brand2', price: '30.00GBP', quantity: 1 },
+  ]);
 
   beforeEach(async () => {
+    const productsServiceSpy = jasmine.createSpyObj('ProductsService', [], {
+      purchasedProducts$: mockProductList,
+    });
+
     await TestBed.configureTestingModule({
       declarations: [PurchasedProductsComponent],
-      providers: [ProductsService],
+      providers: [{ provide: ProductsService, useValue: productsServiceSpy }],
     }).compileComponents();
 
-    productsService = TestBed.inject(ProductsService);
     fixture = TestBed.createComponent(PurchasedProductsComponent);
     component = fixture.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit', () => {
+    it('should set @purchasedProducts$', () => {
+      component.ngOnInit();
+
+      expect(component.purchasedProducts$).toBe(mockProductList);
+    });
   });
 });
